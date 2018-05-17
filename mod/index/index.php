04d6fa0a6ddb554d3cf3941493d105778c2defa3
ozaman <ozaclever@gmail.com>
2018-05-17 10:49:36 +0700

    c

--- mod/index/index.php
+++ mod/index/index.php
@@ -477,7 +477,7 @@
      var url_load= "empty_style.php?name=shop&file=select_province_new&id=1&lat=<?=$arr[shop][lat]?>&lng=<?=$arr[shop][lng]?>&type=stop";
       $('#body_load_select_pv').html(load_main_mod);
       $.post( url_load, function( data ) {
-//      	   $('#body_load_select_pv').html(data);
+      	   $('#body_load_select_pv').html(data);
    	   var txt = $('#province_text').text();
    		$('#txt_pv_fr').val(txt);
    		$('.text-change-province').text(txt);
