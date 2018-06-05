7b672aab6a0fd5acd8c7e4992fea1acf1710f365
ozaman <ozaclever@gmail.com>
2018-06-05 16:29:29 +0700

    c

--- mod/tbooking/all.php
+++ mod/tbooking/all.php
@@ -40,6 +40,7 @@ function thai_date($time){
    right:  0px;
    background-color: #fff;
    margin-top: -25px;
+   border-radius: 25px; 
    }
    @media screen and (max-width: 320px) {
    .font-22{
@@ -69,7 +70,7 @@ function thai_date($time){
    cursor: pointer;
    }
    .btn_filter{
-   padding: 5px; 
+   padding: 8px; 
    border: 1px solid #3b5998; 
    border-radius: 25px;
    /*	width: 100px;*/
@@ -261,12 +262,12 @@ function thai_date($time){
     	width: 100%;
 	}
    .box_his,.box_book{
-   padding: 5px 0px;
+   padding: 5px 8px;
    border: 1px solid #3b5998;
    /*    margin-bottom: 10px;*/
 /*   margin: 30px 0px;*/
    margin: 30px 0px;
-   border-radius: 8px;
+   border-radius: 25px;
    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12), 0 3px 1px -2px rgba(0,0,0,.2);
    }
    .mof{
