1c8ad9b975a351058a994b5dcfd26a442510054c
5540011055 <izudsuarez@gmail.com>
2018-09-17 18:34:23 +0700

    xxx

--- application/views/bank/bank_view.php
+++ application/views/bank/bank_view.php
@@ -2,3 +2,17 @@
     <ons-button style="background-color: #fff;" modifier="outline" class="button-margin button button--outline button--large" onclick="addBank();">เพิ่มข้อมูลบัญชี</ons-button>
 </div>
 
+<?php 
+		$sql = "SELECT * FROM web_bank_driver";
+      	$query_bank = $this->db->query($sql);
+      	
+      	foreach($query_bank->result()  as $row){ ?>
+			<ons-card class="card">
+				<div>
+					
+				</div>
+			</ons-card>
+<?php		}
+?>
+
+
