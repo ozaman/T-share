795db7a504d4f21c310f61e7df577243710147b2
5540011055 <izudsuarez@gmail.com>
2018-09-18 18:34:37 +0700

    xxx

--- application/views/statement/shop_ic.php
+++ application/views/statement/shop_ic.php
@@ -15,25 +15,11 @@
 		    "11"=>"พฤศจิกายน",
 		    "12"=>"ธันวาคม"                 
 		);
-		function thai_date($time,$type){
-		    global $thai_day_arr,$thai_month_arr;
-		    $thai_date_return="วัน".$thai_day_arr[date("w",$time)];
-		    $thai_date_return.= "ที่ ".date("j",$time);
-		    $thai_date_return.=" เดือน".$thai_month_arr[date("n",$time)];
-		    $thai_date_return.= " พ.ศ.".(date("Yํ",$time)+543);
-		    $thai_date_return.= "  ".date("H:i",$time)." น.";
-		    if($type=="y"){
-				return "พ.ศ.".(date("Yํ",$time)+543);
-			}else if($type=="m"){
-				return "เดือน".$thai_month_arr[date("n",$time)];
-			}else if($type=="d"){
-				return "วันที่ ".date("j",$time);
-			}else{
-				return $thai_date_return;
-			}
-		}
-		$select = "SELECT * FROM order_booking where drivername = '".$_COOKIE[detect_user]."' order by transfer_date desc ";
-		$query = $this->db->query($select); ?>
+		
+		$select = "SELECT * FROM order_booking where drivername = '".$_COOKIE[detect_user]."' and MONTH(transfer_date) = '".date('m')."' order by transfer_date desc ";
+		$query = $this->db->query($select);
+		
+		 ?>
 
 <ons-card class="card" style="margin-bottom: 20px">
   <ons-list-item class="input-items list-item p-l-0">
@@ -43,27 +29,41 @@
             <div class="center list-item__center" style="background-image: none;">
                  <label class="center list-item__center" onclick="fn.pushPage({'id': 'option.html', 'title': 'จังหวัด', 'open':'user_province'}, 'lift-ios')">
 	                <span id="txt_user_province" style="color: #000;margin-left: 0px;" >เดือน <?=$thai_month_arr[date("n",time())]." "."พ.ศ.".(date("Yํ",time())+543);?></span>
-	                	<br/>
 	                 <input type="hidden" name="province" id="province" value="" />
 	            </label>
-                <span style="color: #afafaf;  font-size: 13px;   position: absolute;  right: 45px;">เลือกวันที่</span>  
+                <span style="color: #afafaf;  font-size: 13px;   position: absolute;  right: 45px;">เลือกเดือน</span>  
             </div>
             
         </ons-list-item>
 <!-- <input type="date" class="form-control" value="" name="date_report" id="date_report" > -->
 </ons-card>	
+<ons-list>
+<?php	
+		$befordate = '';
+		$i = 0;
+		foreach ($query->result() as $row){ 
+		
+			$tras_d_time = date_create($row->transfer_date);
 
-<?php	foreach ($query->result() as $row){ ?>
-       	
-			 <ons-list>
-			    <ons-list-header style="font-size: 12px;font-weight: 500;"><?=thai_date(strtotime($row->transfer_date),'d');?></ons-list-header>
-			    <ons-list-item style="padding-left: 16px;">Item A</ons-list-item>
-			    <ons-list-item style="padding-left: 16px;">Item B</ons-list-item>
-			</ons-list>
+			if($befordate != $row->transfer_date){ 
+				$befordate = $row->transfer_date;
+				
+				?>
+		<ons-list-header style="font-size: 12px;font-weight: 500;"><?="วันที่ ".date_format($tras_d_time,"Y-m-d");?></ons-list-header>
+<?php			}
+
+?>
+       	<ons-list-item style="padding-left: 16px;"><?=$row->id;?></ons-list-item>
+			
 		
 <?php		}
 ?>
-
+</ons-list>
+<!-- <ons-list>
+			<ons-list-header style="font-size: 12px;font-weight: 500;"><?="วันที่ ".date_format($tras_d_time,"Y-m-d");?></ons-list-header>
+			<ons-list-item style="padding-left: 16px;">Item A</ons-list-item>
+			<ons-list-item style="padding-left: 16px;">Item B</ons-list-item>
+	</ons-list>-->
 <script>
 	function fileterDate(id){
 		console.log($('#'+id).val());
