ae37a74794de3aa601239d8591437e09940a9549
5540011055 <izudsuarez@gmail.com>
2018-09-20 13:25:46 +0700

    xx

--- application/views/statement/trans_ic.php
+++ application/views/statement/trans_ic.php
@@ -6,23 +6,23 @@
 	<?php	
 		$befordate = '';
 		foreach ($_POST[data] as $row){ 
-			$tras_d_time = date_create($row->transfer_date);
-
-			if($befordate != $row->transfer_date){ 
-				$befordate = $row->transfer_date;
+			$total = intval($row[cost])-intval($row[s_cost]);
+//			$tras_d_time = date_create($row->transfer_date);
+			if($befordate != $row[ondate]){ 
+				$befordate = $row[ondate];
 				
 				?>
-		<ons-list-header style="font-size: 12px;font-weight: 500;"><?="วันที่ ".date_format($tras_d_time,"Y-m-d");?></ons-list-header>
+		<ons-list-header style="font-size: 12px;font-weight: 500;"><?="วันที่ ".$row[ondate];?></ons-list-header>
 <?php			}	?>
        <div style="border-bottom: 0px solid #ccc; padding: 15px 5px;" onclick="openDetailOrder('<?=$row->id;?>', '<?=$row->invoice;?>');">
        		<table width="100%">
        			<tr>
-       				<td width="70"><?=$row->invoice;?></td>
+       				<td width="70"><?=$row[invoice];?></td>
        				<td>
-       					<span><?=$row->product_name;?></span><br/>
-       					<span class="font-14"><?=date('Y-m-d h:i',$row->post_date);?></span>
+       					<span class="font-16"><?=$row[pickup_place][topic];?></span><br/>
+       					<span class="font-16"><?=$row[to_place][topic];?></span>
        				</td>
-       				<td align="right"><b><?="+ ".number_format($row->price_all_total,2);?></b></td>
+       				<td align="right"><b><?="+ ".number_format($total,2);?></b></td>
        			</tr>
        		</table>
        </div>
