65d141e288ef67bb0c84afbbea04507c0eb959c9
5540011055 <izudsuarez@gmail.com>
2018-10-31 16:05:57 +0700

    asd

--- assets/script/taxilist.js
+++ assets/script/taxilist.js
@@ -1,13 +1,15 @@
-/*function openDetailDv(id,name){
+function openDetailDv(id,name){
 		console.log(id);
+		fn.pushPage({
+	        'id': 'popup2.html',
+	        'title': name
+    	}, 'lift-ios');
+		$.post("page/call_page?id="+id, {
+	        path: "page/taxi_profile"
+	    }, function(ele) {
+	        $('#body_popup2').html(ele);
+	    });
 		
-		$('#material_dialog_lg').modal('open');
-		$('#dialoglLabel_lg').text(name);
-		$('#load_modal_body').html('<br/><br/><br/>');
-		var url = "empty_style.php?name=taxi_list&file=driver_detail";
-		$.post(url,{ id : id },function(res){
-			$('#load_modal_body_lg').html(res);
-		});
 	}
 function viewDocument(dv_id,type,caption){
 
@@ -17,7 +19,7 @@ function viewDocument(dv_id,type,caption){
 		var load_img = "mod/taxi_list/doc_dv_swiper.php?type="+type+"&driver_id="+dv_id+"&caption="+caption;
 		console.log(load_img);
 		$('#img01').load(load_img);
-}*/
+}
 
 function searchDvName(txt_ip){
 		console.log(txt_ip);
