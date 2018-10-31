03c2048a17817744c20e3ffcb2be371ad852e4f7
5540011055 <izudsuarez@gmail.com>
2018-10-31 18:54:36 +0700

    ๅ/-

--- application/views/page/taxi_list.php
+++ application/views/page/taxi_list.php
@@ -1,34 +1,49 @@
 <?php 
 	$query = $this->db->query("select id, name, nickname, phone from web_driver where status = 1 and id != '".$_COOKIE[detect_user]."' order by id desc");
 	$index = 0;
- 	foreach ($query->result() as $key1=>$row){
+	$num = $query->num_rows();
+	
+	foreach ($_POST[online] as $key=>$val){
+		$arr_post[$val] = $val;
+	}
+	
+ 	foreach ($query->result() as $row){
  		
- 		foreach($_POST[online] as $key2=>$value){
+ 		if($arr_post[$row->id]==$row->id){
+			$data[$index] = $row;
+		}else{
+			$data2[$index] = $row;
+		}
+ 		/*foreach($_POST[online] as $value){
 			if($row->id==$value){
 				$data[$index] = $row;
 			}else{
 				$data2[$index] = $row;
 			}
-		}
+		}*/
  		
  		$index++;
  	}
- 	
+// 	echo json_encode($_POST[online][0]);
 ?>
 <div style="padding: 5px 6px;">
 	<ons-row>
 	  <ons-col style="padding: 5px;"><input type="text" placeholder="ค้นหาจากชื่อ" value="" class="font-17 search-taxi-list" id="search_dv_name" onkeyup="searchDvName(this.value);"  /></ons-col>
 	  <ons-col style="padding: 5px;"><input type="text" placeholder="ค้นหาจากเบอร์โทร" value="" class="font-17 search-taxi-list" id="search_dv_phone" onkeyup="searchDvPhone(this.value);"  /></ons-col>
 	</ons-row>
+	<h3>จำนวนสมาชิก <?=$num;?> คน</h3>
 </div>
+
 <?php 
 	if(count($data)>0){ ?>
 		<ons-list>
 			<ons-list-header>ออนไลน์</ons-list-header>
-			<?php foreach ($data as $row){ ?>
+			<?php 
+			$run_num = 1 ;
+			foreach ($data as $key=>$row){ ?>
 		    <ons-list-item style="padding: 0 0 0 10px;" id="list_id_<?=$row->id;?>" onclick="openDetailDv('<?=$row->id;?>','<?=$row->nickname;?>');">
 		      <div class="left">
-		        <ons-icon icon="md-face" style="font-size: 26px;" class="list-item__icon" id="icon_online_<?=$row->id;?>"></ons-icon>
+		        <span style="width: 30px;"><?=$run_num++;?></span>&nbsp;<ons-icon icon="md-face" style="font-size: 26px;" class="list-item__icon" id="icon_online_<?=$row->id;?>"></ons-icon>
 		      </div>
 		      <div class="center">
 		        <span class="sp_name" role="<?=$row->id;?>"><?=$row->name;?>&nbsp;<?=$row->nickname;?></span>
@@ -43,13 +58,14 @@
 ?>
 
 <?php 
+//	$run_num2 = 1 ;
 	if(count($data2)>0){ ?>
 		<ons-list>
 			<ons-list-header>ออฟไลน์</ons-list-header>
-			<?php foreach ($data2 as $row){ ?>
+			<?php foreach ($data2 as $key2=>$row){ ?>
 		    <ons-list-item style="padding: 0 0 0 10px;" id="list_id_<?=$row->id;?>" onclick="openDetailDv('<?=$row->id;?>','<?=$row->nickname;?>');">
 		      <div class="left">
-		        <ons-icon icon="md-face" style="font-size: 26px;" class="list-item__icon" id="icon_online_<?=$row->id;?>"></ons-icon>
+		       <span style="width: 30px;"><?=$run_num++;?></span> <ons-icon icon="md-face" style="font-size: 26px;" class="list-item__icon" id="icon_online_<?=$row->id;?>"></ons-icon>
 		      </div>
 		      <div class="center">
 		        <span class="sp_name" role="<?=$row->id;?>"><?=$row->name;?>&nbsp;<?=$row->nickname;?></span>
@@ -62,3 +78,21 @@
 		</ons-list>
 <?php	}
 ?>
+<!--<ons-list>
+			<ons-list-header>ออฟไลน์</ons-list-header>
+			<?php 
+			$run_num = 1;
+			foreach ($query->result() as $key1=>$row){ ?>
+		    <ons-list-item style="padding: 0 0 0 10px;" id="list_id_<?=$row->id;?>" onclick="openDetailDv('<?=$row->id;?>','<?=$row->nickname;?>');">
+		      <div class="left">
+		       <span style="width: 30px;"><?=$run_num++;?></span> <ons-icon icon="md-face" style="font-size: 26px;" class="list-item__icon" id="icon_online_<?=$row->id;?>"></ons-icon>
+		      </div>
+		      <div class="center">
+		        <span class="sp_name" role="<?=$row->id;?>"><?=$row->name;?>&nbsp;<?=$row->nickname;?></span>
+		      </div>
+		      <div class="right">
+		        <span class="sp_phone"  role="<?=$row->id;?>" ><?=$row->phone;?></span>
+		      </div>
+		    </ons-list-item>
+		    <?php } ?>
+		</ons-list>-->
