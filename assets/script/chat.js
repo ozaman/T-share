f1a1b9ae1e0674fa97d1711806471711f50f232a
ozaman <ozaclever@gmail.com>
2018-11-02 14:51:09 +0700

    cccc

---
+++ assets/script/chat.js
@@ -0,0 +1,20 @@
+function loadcontactChat() {
+	// $('#shop_add').html(ele2);
+            // var pass = {
+            //     date: moment().format('YYYY-MM-DD'),
+            //     driver: $.cookie("detect_user"),
+            //     type: 'his'
+            // };
+            // console.log(pass);
+            // var urlcounthis = ""
+            $.ajax({
+                url: "chat/contactChat",
+                data: pass,
+                type: 'post',
+                success: function(res) {
+                    // console.log(res);
+                    $('#body_contact').html(res)
+                    // $('ons-tab[page="shop_history.html"]').attr('badge', res);
+                }
+            });
+}
