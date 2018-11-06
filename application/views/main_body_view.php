959efbbb960b87543578ad3d8e6c2a7fb32fada5
5540011055 <izudsuarez@gmail.com>
2018-11-06 12:34:42 +0700

    123

--- application/views/main_body_view.php
+++ application/views/main_body_view.php
@@ -2,7 +2,7 @@
 <ons-row style="margin: 10px 0px;">
   <ons-col style="margin: 10px;">
 	  <center>
-	  	<div onclick="beforeSendShop();" class="circle-menu" style="    background-color: #34A0E7;" >
+	  	<div onclick="beforeSendShop();" class="circle-menu-home" style="background-color: #34A0E7;" >
 	      <span id="number_shop" class="badge badge-custom font-18 pulse" style="display: none;">0</span>
 	      <div class="content">
 	     	<i class="icon-new-uniF14D" style="font-size: 24px;position: relative; top: 7px; left: 2px;"></i>
@@ -14,7 +14,7 @@
 
   <ons-col style="margin: 10px;">
    <center>
-	  	<div onclick="sendTransfer();" class="circle-menu" style="    background-color: #F7941D;">
+	  	<div onclick="sendTransfer();" class="circle-menu-home" style="    background-color: #F7941D;">
 	  	  <span id="number_tbooking" class="badge badge-custom font-18 pulse" style="">2</span>
 	      <div class="content">
 	     	<i class="icon-new-uniF10A-9" style="font-size: 28px;position: relative; top: 7px; left: 0px;"></i>
@@ -28,7 +28,7 @@
 <ons-row style="margin: 10px 0px;">
   <ons-col style="margin: 10px;">
 	  <center>
-	  	<div onclick="booking();" class="circle-menu" style="    background-color: #1CC1A4;" >
+	  	<div onclick="booking();" class="circle-menu-home" style="    background-color: #1CC1A4;" >
 	      
 	      <div class="content">
 	     	<i class="fa fa-taxi" style="font-size: 24px;position: relative; top: 7px; left: 0px;"></i>
@@ -40,7 +40,7 @@
 
   <ons-col style="margin: 10px;">
    <center>
-	  	<div onclick="tour();" class="circle-menu" style="    background-color: #8DC63F;">
+	  	<div onclick="tour();" class="circle-menu-home" style="    background-color: #8DC63F;">
 	      <div class="content">
 	     	<i class="fa fa-suitcase" style="font-size: 28px;position: relative; top: 7px; left: 0px;"></i>
 	      </div>
@@ -53,10 +53,10 @@
 <ons-row style="margin: 10px 0px;">
   <ons-col style="margin: 10px;">
 	  <center>
-	  	<div onclick="income();" class="circle-menu" style="    background-color: #3b5998;" >
+	  	<div onclick="income();" class="circle-menu-home" style="    background-color: #3b5998;" >
 	      
 	      <div class="content">
-	     	<i class="icon-new-uniF121-10" style="font-size: 24px;position: relative; top: 7px; left: 0px;"></i>
+	     	<i class="fa fa-usd" style="font-size: 28px;position: relative; top: 7px; left: 0px;"></i>
 	      </div>
 	    </div>
 	    <span class="txt-dark">รายรับ</span>
@@ -65,7 +65,7 @@
 
   <ons-col style="margin: 10px;">
    <center>
-	  	<div onclick="pay()" class="circle-menu" style="    background-color: #e91e63;">
+	  	<div onclick="pay()" class="circle-menu-home" style="    background-color: #e91e63;">
 	      <div class="content">
 	     	<i class="fa fa-usd" style="font-size: 28px;position: relative; top: 7px; left: 0px;"></i>
 	      </div>
